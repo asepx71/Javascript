4f26e9fe30b417cd1a315f5feb78903b81c8dc56
Asep Rohimat <39237395+asepx71@users.noreply.github.com>
2022-04-28 13:58:03 +0700

    Create Anti-kopas2.js

---
+++ Anti-kopas2.js
@@ -0,0 +1,33 @@
+<style>
+#scrCoverRedirect{text-align:center;background-color:#0083da;z-index:+999999999999999999;width:100%;height:100%;position:fixed;top:0;left:0;display:none;}.elmCoverShareFbGroup{top:20%;position:relative;}.btnCoverShareFbGroup{margin-top: 10px;position:relative;display:inline-block;text-decoration:none;border-radius:4px;color:#fff;border:1px solid #666;padding:15px 25px;background:rgba(0,0,0,0.5);-webkit-transition:background 0.15s ease-in-out;-moz-transition:background 0.15s ease-in-out;-ms-transition:background 0.15s ease-in-out;-o-transition:background 0.15s ease-in-out;transition:background 0.15s ease-in-out;}.btnCoverShareFbGroup:hover{background-color:#0083da;color:#fff;}.btnCoverShareFbGroup>.btnShineEffect{position:absolute;opacity:0;top:0;left:0;background:-moz-linear-gradient(left,rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%);background:-webkit-gradient(linear,left top,right top,color-stop(0%,rgba(255,255,255,0)),color-stop(1%,rgba(255,255,255,0.03)),color-stop(30%,rgba(255,255,255,0.85)),color-stop(50%,rgba(255,255,255,0.85)),color-stop(70%,rgba(255,255,255,0.85)),color-stop(71%,rgba(255,255,255,0.85)),color-stop(100%,rgba(255,255,255,0)));background:-webkit-linear-gradient(left,rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%);background:-o-linear-gradient(left,rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%);background:-ms-linear-gradient(left,rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%);background:linear-gradient(to right,rgba(255,255,255,0) 0%,rgba(255,255,255,0.03) 1%,rgba(255,255,255,0.6) 30%,rgba(255,255,255,0.85) 50%,rgba(255,255,255,0.85) 70%,rgba(255,255,255,0.85) 71%,rgba(255,255,255,0) 100%);filter:progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff',endColorstr='#00ffffff',GradientType=1 );width:15%;height:100%;transform:skew(-10deg,0deg);-webkit-transform:skew(-10deg,0deg);-moz-transform:skew(-10deg,0deg);-ms-transform:skew(-10deg,0deg);-o-transform:skew(-10deg,0deg);animation:move 2s;animation-iteration-count:infinite;animation-delay:1s;-webkit-animation:move 2s;-webkit-animation-iteration-count:infinite;-webkit-animation-delay:1s;-moz-transform:skew(-10deg,0deg);-moz-animation:move 2s;-moz-animation-iteration-count:infinite;-moz-animation-delay:1s;-ms-transform:skew(-10deg,0deg);-ms-animation:move 2s;-ms-animation-iteration-count:infinite;-ms-animation-delay:1s;-o-transform:skew(-10deg,0deg);-o-animation:move 2s;-o-animation-iteration-count:infinite;-o-animation-delay:1s;}
+</style>
+<style>
+@keyframes move{0%{left:0;opacity:0;}5%{opacity:0.0;}48%{opacity:0.2;}80%{opacity:0.0;}100%{left:92%;}}
+@-webkit-keyframes move{0%{left:0;opacity:0;}5%{opacity:0.0;}48%{opacity:0.2;}80%{opacity:0.0;}100%{left:92%;}}
+@-moz-keyframes move{0%{left:0;opacity:0;}5%{opacity:0.0;}48%{opacity:0.2;}80%{opacity:0.0;}100%{left:88%;}}
+@-ms-keyframes move{0%{left:0;opacity:0;}5%{opacity:0.0;}48%{opacity:0.2;}80%{opacity:0.0;}100%{left:92%;}}
+@-o-keyframes move{0%{left:0;opacity:0;}5%{opacity:0.0;}48%{opacity:0.2;}80%{opacity:0.0;}100%{left:92%;}}
+</style>
+
+<script>
+//<![CDATA[
+var tmpNRghtClick='<div id="scrCoverRedirect"><div class="elmCoverShareFbGroup"><img src="" border="0"/></div>';
+$("body").append(tmpNRghtClick);
+function addEvent(obj, evt, fn) {
+if (obj.addEventListener) {
+obj.addEventListener(evt, fn, false);
+} else if (obj.attachEvent) {
+obj.attachEvent("on" + evt, fn);
+}}
+addEvent(window, "load", function(e) {
+addEvent(document, "mouseout", function(e) {
+e = e ? e : window.event;
+var from = e.relatedTarget || e.toElement;
+if (!from || from.nodeName == "HTML") {
+setTimeout(function(){window.location.assign("http://codeflare.6te.net/Protection.html");},500);
+$("#scrCoverRedirect").show("blind");
+if($('.elmCoverShareFbGroup img').attr('src')==''){
+$('.elmCoverShareFbGroup img').attr('src','https://1.bp.blogspot.com/-L7vJE650ZV0/X50FEzsYhaI/AAAAAAAADGM/-ZljwmzxIhg_JikIcacGLofsCWwAl__4wCLcBGAsYHQ/s256/codeflare-hacker-512.png');
+}}});});
+//]]>
+</script>
